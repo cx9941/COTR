ab97f7a4bee7a3fd1ccfb0ea0590d4ab9b3f2506
cx9941 <cx9941@gmail.com>
2024-10-09 17:18:11 +0000

    add baseline

--- step5-baseline/llm_func.py
+++ step5-baseline/llm_func.py
@@ -50,8 +50,8 @@ def get_ebot_results(prompt):
     # 通过环境变量初始化认证信息
     # 方式一：【推荐】使用安全认证AK/SK鉴权
     # 替换下列示例中参数，安全认证Access Key替换your_iam_ak，Secret Key替换your_iam_sk，如何获取请查看https://cloud.baidu.com/doc/Reference/s/9jwvz2egb
-    os.environ["QIANFAN_ACCESS_KEY"] = "ALTAKAPLmViDOdQsF3NMPPQtx6"
-    os.environ["QIANFAN_SECRET_KEY"] = "61a662aab7314cecb20a122a77109fcd"
+    os.environ["QIANFAN_ACCESS_KEY"] = "your_iam_ak"
+    os.environ["QIANFAN_SECRET_KEY"] = "your_iam_sk"
     chat_comp = qianfan.ChatCompletion()
     # 指定特定模型
     resp = chat_comp.do(model="ERNIE-Speed-8K", messages=[{
